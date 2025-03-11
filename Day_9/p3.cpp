0ea8bf9330290b7561eff5f505e892937ce6e1fc
deepakydv641 <deepakydv26422@gmail.com>
2025-03-11 13:34:28 +0530

    Update p3.cpp

--- Day_9/p3.cpp
+++ Day_9/p3.cpp
@@ -1,1 +1,16 @@
+// Maximum Score from Subarray Minimums
+class Solution {
+    public:
+      
+      int pairWithMaxSum(vector<int> &arr) {
+          int n = arr.size();
+          int max_sum =0;
+          for(int i=0;i<n-1;i++){
+              if(arr[i]+arr[i+1]>max_sum){
+                  max_sum=arr[i]+arr[i+1];
+              }
+          }
+          return max_sum;
+      }
+  };
 
