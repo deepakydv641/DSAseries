00dde9118ff0c70947a70a080f58279f7a595e10
deepakydv641 <deepakydv26422@gmail.com>
2025-03-11 08:59:39 +0530

    Update p2.cpp

--- Day_9/p2.cpp
+++ Day_9/p2.cpp
@@ -1,1 +1,17 @@
-
+// Sort Colors
+class Solution {
+    public:
+        void sortColors(vector<int>& nums) {
+            for(int i=0;i<nums.size();i++){
+                for(int j=i+1;j<nums.size();j++){
+                    int temp=0;
+                         if(nums[i]>nums[j]){
+                            swap(nums[i],nums[j]);
+                         }
+                }
+                        }
+                        for(int i=0;i<nums.size();i++){
+                            cout<<nums[i];
+                        }
+        }
+    };
