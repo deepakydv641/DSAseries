bc97f44999c1397eab47233f126235dca6400437
deepakydv641 <deepakydv26422@gmail.com>
2025-03-12 20:26:14 +0530

    Update p1.cpp

--- Day_11/p1.cpp
+++ Day_11/p1.cpp
@@ -1,1 +1,19 @@
-
+//Pascal Triangle
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>v;
+        for(int i=0;i<numRows;i++){
+            vector<int>k;
+            for(int j=0;j<=i;j++){
+                if(i==j||j==0){
+                    k.push_back(1);
+                }
+                else{
+                    k.push_back(v[i-1][j]+v[i-1][j-1]);
+                }
+            }
+            v.push_back(k);
+        }
+    return v;}
+};
