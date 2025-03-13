d9b3fd44d4be502618cffa393b9ceb313265632f
deepakydv641 <deepakydv26422@gmail.com>
2025-03-13 11:17:35 +0530

    Update p1.cpp

--- Day_12/p1.cpp
+++ Day_12/p1.cpp
@@ -1,1 +1,28 @@
-
+// Binary Search
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+      
+     
+      return bs(nums, target, 0, nums.size() - 1);
+    
+    
+      }
+      private:
+         int bs(vector<int>& nums, int target, int start, int end){
+          
+           while(start<=end){
+            int mid=(start+end)/2;
+            if(target==nums[mid]){
+                return mid;
+            }
+            if(nums[mid]<target){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+           }
+           return -1;
+        }
+};
