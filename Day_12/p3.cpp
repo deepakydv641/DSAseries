bfded59196e25a7c979d2beb663a66fc349c5f3f
deepakydv641 <deepakydv26422@gmail.com>
2025-03-13 14:34:42 +0530

    Update p3.cpp

--- Day_12/p3.cpp
+++ Day_12/p3.cpp
@@ -1,1 +1,34 @@
-
+//Search Insert Position
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        if(bs(nums,0,nums.size()-1,target)==-1){
+            for(int i=0;i<nums.size();i++){
+                if(nums[i]>target){
+                    return i;
+                }
+            }
+            return nums.size();
+        }
+        else{
+            return bs(nums,0,nums.size()-1,target);
+        }
+    return 0;}
+    private: int bs(vector<int> & arr,int start,int end,int target){
+        while(start<=end){
+            int k;
+            int mid=(start+end)/2;
+            if(target==arr[mid]){
+                return mid;
+            }
+            else if(target<arr[mid]){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+           
+        } 
+       return -1; 
+    }
+};
