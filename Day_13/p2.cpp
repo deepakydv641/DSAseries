eba360994736b79f0ff7c00616aee024132fae50
deepakydv641 <deepakydv26422@gmail.com>
2025-03-14 18:52:22 +0530

    Update p2.cpp

--- Day_13/p2.cpp
+++ Day_13/p2.cpp
@@ -1,1 +1,17 @@
-
+// Search in Rotated Sorted Array
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int flag=0;
+   for(int i=0;i<nums.size();i++){
+      if(nums[i]==target){
+        flag=1;
+        return i;
+        break;
+      }
+   }
+   if(flag==0){
+    return -1;
+   }
+  return 0; }
+};
