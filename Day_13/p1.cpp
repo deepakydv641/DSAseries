95143a5c13adac2eee8c577e724286d71d675c03
deepakydv641 <deepakydv26422@gmail.com>
2025-03-14 20:20:36 +0530

    Update p1.cpp

--- Day_13/p1.cpp
+++ Day_13/p1.cpp
@@ -1,1 +1,16 @@
-
+// Ceil The Floor
+pair<int, int> getFloorAndCeil(vector<int> &a, int n, int x) {
+	for(int i=0;i<n;i++){
+		if(a[i]==x){
+			return {x,x};
+		}
+		else if(a[i]>x&&i>0){
+			return {a[i-1],a[i]};
+		}
+		else if(a[i]>x&&i==0){
+			return {-1,a[i]};
+		}else if(i==n-1&&a[i]<x){
+			return {a[i],-1};
+		}
+	}
+}
