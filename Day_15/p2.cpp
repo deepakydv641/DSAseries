054e4acfba40ff8ec3cd75bf9d6f58a8e1cfef28
deepakydv641 <deepakydv26422@gmail.com>
2025-03-18 20:49:55 +0530

    Update p2.cpp

--- Day_15/p2.cpp
+++ Day_15/p2.cpp
@@ -1,1 +1,19 @@
-
+// Search a 2D Matrix II
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row=0;
+        int col=matrix[0].size()-1;
+        while(row<matrix.size()&&col>=0){
+            if(target==matrix[row][col]){
+                return true;
+            }
+            else if(target<matrix[row][col]){
+                col--;
+            }
+            else{
+                row++;
+            }
+        }
+   return false; }
+};
