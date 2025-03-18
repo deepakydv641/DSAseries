1f4b4a4a6ed504e4c4e61315c8f4cf40a4e3e1e2
deepakydv641 <deepakydv26422@gmail.com>
2025-03-18 21:04:16 +0530

    Update p3.cpp

--- Day_15/p3.cpp
+++ Day_15/p3.cpp
@@ -1,1 +1,26 @@
-
+//K-th element of two Arrays
+class Solution {
+  public:
+    int kthElement(vector<int>& a, vector<int>& b, int k) {
+        int i=0,j=0;
+        vector<int>v;
+        while(i<a.size()&&j<b.size()){
+            if(a[i]<b[j]){
+                v.push_back(a[i]);
+                i++;
+            }
+            else{
+                v.push_back(b[j]);
+                j++;
+            }
+        }
+        while(i<a.size()){
+            v.push_back(a[i]);
+            i++;
+        }
+        while(j<b.size()){
+            v.push_back(b[j]);
+            j++;
+        }
+    return v[k-1];}
+};
