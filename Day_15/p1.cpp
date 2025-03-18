fc92815a7717fa1398ac1d9068f0ec76e4deefd9
deepakydv641 <deepakydv26422@gmail.com>
2025-03-18 19:00:42 +0530

    Update p1.cpp

--- Day_15/p1.cpp
+++ Day_15/p1.cpp
@@ -2,21 +2,23 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        vector<int>v;
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[i].size();j++){
-                v.push_back(matrix[i][j]);
-            }
+    
+    
+    
+    for(int i=0;i<matrix.size();i++){
+       if(bs(matrix[i],target,0,matrix[i].size()-1)==1){
+        return true;
+       }
+       
         }
-       return bs(v,target,0,v.size()-1); 
-    }
+    return false;}
 
        
 public:  bool bs(vector<int>& matrix, int target,int start,int end){
                 if(start<=end){
                     int mid=(start+end)/2;
                     if(matrix[mid]==target){
-                        return true;
+                        return 1;
                     }
                     else if(matrix[mid]<target){
                         return bs(matrix,target,mid+1,end);
@@ -25,7 +27,7 @@ public:  bool bs(vector<int>& matrix, int target,int start,int end){
                          return bs(matrix,target,start,mid-1);
                     }
                }
-         return false;     }
+         return 0;     }
         
     
 };
