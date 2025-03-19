0209ed5000fc026f02b1f8da55c435c7f6501a1a
deepakydv641 <deepakydv26422@gmail.com>
2025-03-19 13:55:21 +0530

    Update p2.cpp

--- Day_17/p2.cpp
+++ Day_17/p2.cpp
@@ -1,1 +1,29 @@
-
+//Remove Outermost Parentheses
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans;
+        int sum=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]==')'){
+                sum+=-1;
+                if(sum!=0){
+                 ans+=s[i];}
+            }
+            else if(s[i]=='('){
+               if(sum==0){
+                    sum+=1;
+                    
+                }
+                else if(sum!=0){
+                    sum+=1;
+                    ans+=s[i];
+                }
+                else if(sum!=0&&i>0){
+                ans+=s[i];}
+            }
+            
+        }
+        
+    return ans;}
+};
