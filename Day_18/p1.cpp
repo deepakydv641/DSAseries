c958cfc1c30407942b4d167b331006337d205ca6
deepakydv641 <deepakydv26422@gmail.com>
2025-03-20 10:31:17 +0530

    Update p1.cpp

--- Day_18/p1.cpp
+++ Day_18/p1.cpp
@@ -1,1 +1,21 @@
-
+//Valid Anagram
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char,int>mpp,mp;
+        for(int i=0;i<s.size();i++){
+            mpp[s[i]]++;
+        }
+        for(int i=0;i<t.size();i++){
+            mp[t[i]]++;
+        }
+        if(s.size()==t.size()){for(int i=0;i<s.size();i++){
+            if(mpp[s[i]]!=mp[s[i]]){
+                return false;
+            }
+        }}
+        else{
+            return false;
+        }
+    return true;}
+};
