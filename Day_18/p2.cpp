84dece7236245de33763ecfc0c5de016805e6dc3
deepakydv641 <deepakydv26422@gmail.com>
2025-03-20 20:48:27 +0530

    Update p2.cpp

--- Day_18/p2.cpp
+++ Day_18/p2.cpp
@@ -1,1 +1,17 @@
-
+//Longest Common Prefix
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        char c;
+        string ans="";
+        for(int i=0;i<strs[0].size();i++){
+            c=strs[0][i];
+            for(int j=0;j<strs.size();j++){
+                if(c!=strs[j][i]){
+                    return ans;
+                }
+            }
+            ans+=c;
+        }
+    return ans;}
+};
