738d56f26448b2a8caa23707c6b6b2b9af7876a8
deepakydv641 <deepakydv26422@gmail.com>
2025-03-21 09:40:49 +0530

    Update p2.cpp

--- Day_19/p2.cpp
+++ Day_19/p2.cpp
@@ -1,1 +1,22 @@
+// Maximum Nesting Depth of the Parentheses
+class Solution {
+public:
+    int maxDepth(string s) {
+        stack<char> st;
+        int maxi=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                st.push('(');
+                int x=st.size();
+                maxi=max(maxi,x);
+            }
+            else if(s[i]==')'){
+                st.pop();
+            }
+            
+        }
+    return maxi;}
+};
 
+//T(c)=o(n);
+// s(c)=o(n)
