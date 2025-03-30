cf204a8f9cc4f9194d75aab500dfb7dfd4569ece
deepakydv641 <deepakydv26422@gmail.com>
2025-03-30 21:21:38 +0530

    Create p1.cpp

---
+++ Day_27/p1.cpp
@@ -0,0 +1,25 @@
+//Reverse a Doubly Linked List
+class Solution {
+  public:
+    // Function to reverse a doubly linked list
+    DLLNode* reverseDLL(DLLNode* head) {
+        stack<int>st;
+        DLLNode*temp=head;
+        while(temp!=NULL){
+            st.push(temp->data);
+            temp=temp->next;
+        }
+        DLLNode*dummy=new DLLNode(0);
+        dummy->next=head;
+        head->prev=dummy;
+        DLLNode*peak=dummy;
+        while(!st.empty()){
+            DLLNode*nn=new DLLNode(st.top());
+            nn->prev=peak;
+            nn->next=NULL;
+            peak->next=nn;
+            peak=peak->next;
+            st.pop();
+        }
+    return dummy->next;}
+};
