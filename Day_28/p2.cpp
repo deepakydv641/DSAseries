3f518a07bbf17f92c3c169837d57ddc95564a572
deepakydv641 <deepakydv26422@gmail.com>
2025-03-31 15:06:10 +0530

    Update p2.cpp

--- Day_28/p2.cpp
+++ Day_28/p2.cpp
@@ -1,4 +1,26 @@
 //Middle of the Linked List
+
+//approach-1
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode*slow=head;
+        ListNode*fast=head;
+        while(fast!=NULL){
+            if(fast->next==NULL||fast==NULL){
+                return slow;
+                break;
+            }
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+    return slow;}
+};
+
+//T(c)=O(n)
+//S(c)=O(1)
+
+//approach-2
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
