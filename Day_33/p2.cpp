543019693096b9eab5c4f1aa32aaec656c7118ea
deepakydv641 <deepakydv26422@gmail.com>
2025-04-15 23:10:02 +0530

    Update p2.cpp

--- Day_33/p2.cpp
+++ Day_33/p2.cpp
@@ -1,4 +1,41 @@
-//
+//Copy List with Random Pointer:  https://leetcode.com/problems/copy-list-with-random-pointer/ 
+
+// approach-1
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        Node*temp=head;
+        while(temp!=NULL){
+            Node*nn=new Node(temp->val);
+            nn->next=temp->next;
+            temp->next=nn;
+            temp=temp->next->next;
+        }
+        temp=head;
+        while(temp!=NULL){
+            Node*cpy=temp->next;
+           if(temp->random!=NULL){cpy->random=temp->random->next;}
+           else{
+            cpy->random=NULL;
+           }
+            temp=temp->next->next;
+        }
+        temp=head;
+        Node*dummy=new Node(0);
+        Node*list=dummy;
+        while(temp!=NULL){
+            Node*nn=temp->next;
+            list->next=temp->next;
+            list=nn;
+            temp->next=list->next;
+            temp=temp->next;
+        }
+    return dummy->next;}
+};
+
+//  T(c)=O(n)
+// S(c)=O(1)
+
 
 //approach-2
 class Solution {
