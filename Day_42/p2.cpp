d0ec4a24b25456683ec31399fa62d7e52a1957cc
deepakydv641 <deepakydv26422@gmail.com>
2025-04-29 00:30:07 +0530

    Create p2.cpp

---
+++ Day_42/p2.cpp
@@ -0,0 +1,19 @@
+//Minimum Bit Flips to Convert Number
+
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        if(goal==start){
+            return 0;
+        }
+        int c=start^goal;
+        int cnt=0;
+        while(c>2){
+            if(c%2==1){
+                cnt++;
+            }
+            c=c/2;
+        }
+        return cnt+1;
+    }
+};
